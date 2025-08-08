bc96f6381a984adf2d9814aba4910664dc089fd6
Likhith Kotha <93262389+Likhith02@users.noreply.github.com>
2025-08-08 11:43:20 -0500

    Update Score.js

--- backend/models/Score.js
+++ backend/models/Score.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-
 const scoreSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   xp: { type: Number, default: 0 },
   best: { type: Number, default: 0 },
 });
-
 module.exports = mongoose.model('Score', scoreSchema);
